04556db2359e0ef4671532d74f012c5578cb3cbf
Sakshi Chauahan <you@example.com>
2022-03-20 11:27:47 +0530

    js changes

--- script.js
+++ script.js
@@ -8,10 +8,30 @@ var btn = document.getElementsByClassName('grid-item');
 // }
 
 function btnClick(ele) {
+    var x = 0, y = 0, operator = 0, ans = 0, flag = 0, s = "", op = 0;
     if(ele.id == 'ac') {
         scr.innerHTML = "";
     } else if(ele.id== 'ce') {
         scr.innerHTML = "";
+    } else if(ele.classList.contains('op')) {
+        for(var i = 0; i <scr.length; i++) {
+            x += parseFloat(scr[i].innerHTML);
+        }
+
+        operator = ele.innerHTML;
+        scr.innerHTML += ele.innerHTML;
+        flag = 1;
+    } else if(flag == 1) {
+        s += ele.innerHTML
+        scr.innerHTML += ele.innerHTML;
+    } else if(flag == 1 && ele.id == 'equal') {
+        for(var i = 0; i < y.length; i++) {
+            y += parseFloat(y[i].innerHTML);
+        }
+        op = parseFloat(operator);
+        ans = x op y;
+        screen.innerHTML = ans;
+        flag = 0;
     } else {
         scr.innerHTML += ele.innerHTML;
     }
