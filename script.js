3587d2fb65d3e0b695b985dce686a6bf5a8e1308
Sakshi Chauahan <you@example.com>
2022-03-20 12:38:50 +0530

    working js code

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 var scr = document.getElementById('screen');
 var btn = document.getElementsByClassName('grid-item');
-
+var brn = document.getElementsByClassName('burner');
+brn.innerHTML = "";
 // for(var i = 0; i < btn.length; i++) {
 //     btn[i].addEventListener("click", ()=> {
 //         console.log(btn);
 //     });
 // }
-
+var x = 0, y = 0, operator = "", ans = 0, flag = 0, op = 0;
 function btnClick(ele) {
-    var x = 0, y = 0, operator = 0, ans = 0, flag = 0, s = "", op = 0;
+    
     if(ele.id == 'ac') {
         scr.innerHTML = "";
     } else if(ele.id== 'ce') {
         scr.innerHTML = "";
-    } else if(ele.classList.contains('op')) {
-        for(var i = 0; i <scr.length; i++) {
-            x += parseFloat(scr[i].innerHTML);
-        }
-
+    } else if(ele.classList.contains('op') && flag != 1) {
+        x = parseFloat(scr.innerHTML);
         operator = ele.innerHTML;
         scr.innerHTML += ele.innerHTML;
         flag = 1;
-    } else if(flag == 1) {
-        s += ele.innerHTML
+        // console.log(x);
+    } else if(flag == 1 && ele.id != 'equal') {
+        // console.log("hello");
+        brn.innerHTML += ele.innerHTML;
         scr.innerHTML += ele.innerHTML;
+        // console.log(brn.innerHTML);
     } else if(flag == 1 && ele.id == 'equal') {
-        for(var i = 0; i < y.length; i++) {
-            y += parseFloat(y[i].innerHTML);
-        }
-        op = parseFloat(operator);
-        ans = x op y;
-        screen.innerHTML = ans;
+        y = parseFloat(brn.innerHTML);
+        // console.log(y);
+        // console.log(ele.id);
+        // op = parseFloat(operator);
+        ans = x + y;
+        console.log(ans);
+        screen.innerHTML = "";
         flag = 0;
+
     } else {
         scr.innerHTML += ele.innerHTML;
     }
+   
+    // else {
+    //     scr.innerHTML += ele.innerHTML;
+    //     brn.innerHTML += scr.innerHTML;
+    //     y = parseFloat(scr.innerHTML);
+    //     console.log(y);
+    // }
     
 }
 
